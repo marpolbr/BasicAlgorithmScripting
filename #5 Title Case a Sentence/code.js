069e20cbe706a29f6606479ff465163d466e712e
Marcin <polbrat.marcin@gmail.com>
2016-11-07 15:37:38 +0100

    #6 Return Largest Numbers in Arrays

--- #5 Title Case a Sentence/code.js
+++ #5 Title Case a Sentence/code.js
@@ -8,3 +8,4 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
---
+++ #6 Return Largest Numbers in Arrays/code.js
@@ -0,0 +1,13 @@
+
+function largestOfFour(arr) {
+  var x = [];
+  for(var i=0; i<arr.length; i++){
+    arr[i].sort(function(a, b) {
+      return b - a;
+    });
+    x.push(arr[i][0]);
+  }
+  return x;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
