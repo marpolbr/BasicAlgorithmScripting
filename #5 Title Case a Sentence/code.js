d69b0b141829292b5d48459458f8c8edcb569d17
Marcin <polbrat.marcin@gmail.com>
2016-11-07 15:15:18 +0100

    #5 Title Case a Sentence

---
+++ #5 Title Case a Sentence/code.js
@@ -0,0 +1,10 @@
+
+function titleCase(str) {
+  var a = str.toLowerCase().split(' ');
+  for(var i=0; i<a.length; i++){
+    a[i] = a[i].charAt(0).toUpperCase() + a[i].slice(1);
+  }
+  return a.join(' ');
+}
+
+titleCase("I'm a little tea pot");
