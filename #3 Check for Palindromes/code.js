d2367b35c641dc5f225b49fb04b000b3d4badfb0
Marcin <polbrat.marcin@gmail.com>
2016-11-08 15:46:48 +0100

    Edit exercises #3 and #7

--- #3 Check for Palindromes/code.js
+++ #3 Check for Palindromes/code.js
@@ -4,16 +4,8 @@ function palindrome(str) {
   var strBefore = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
   var strAfter = strBefore.split('').reverse().join('');
   
-  var pali;
-  if(strBefore===strAfter){
-    pali = true;
-  }
-  else{
-    pali = false;
-  }
-  return pali;
+  return strBefore===strAfter;
+
 }
-
-
 
 palindrome("2_A3*3#A2");
--- #7 Confirm the Ending/code.js
+++ #7 Confirm the Ending/code.js
@@ -1,13 +1,9 @@
 
 function confirmEnding(str, target) {
+
   str = str.substring(str.length - target.length);
-  if(str===target){
-    return true;
-  }
-  else{
-    return false;
-  }
   
+  return str===target;
 }
 
 confirmEnding("He has to give me a new name", "name");
