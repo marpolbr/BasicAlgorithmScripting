b2d99d2fdcd84d2e60eabb7bce26816e51dac234
Marcin <polbrat.marcin@gmail.com>
2016-11-08 16:48:41 +0100

    #9 Truncate a string

---
+++ #9 Truncate a string/code.js
@@ -0,0 +1,16 @@
+
+function truncateString(str, num) {
+    
+  if(num<=3){
+    return str.slice(0,num) + "...";
+  }
+  else if(num<str.length){   
+    return str.slice(0,num-3) + "...";
+  }
+  else if(num>=str.length){
+    return str;
+  }
+  
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
